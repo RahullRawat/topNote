5ec33d5d62f8eaadcfaa456ccd9514632a4e0455
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-03-26 14:28:40 +0530

    feat : added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
-import { Landing } from "./pages/Landing/Landing";
+import { Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Landing, Homepage } from "./pages/index";
 
 function App() {
 	return (
 		<div className="App">
-			<Landing />
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Landing />} />
+				<Route path="/home" element={<Homepage />} />
+			</Routes>
 		</div>
 	);
 }
