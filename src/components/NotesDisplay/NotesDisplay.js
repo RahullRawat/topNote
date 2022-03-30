db0e4895a009b03e8f75ef14f2f5dd9d348e1e91
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-03-30 16:49:00 +0530

    feat : added bgColor variable

--- src/components/NotesDisplay/NotesDisplay.js
+++ src/components/NotesDisplay/NotesDisplay.js
@@ -14,12 +14,25 @@ const NotesDisplay = ({ notes, deleteNotesHandler, notesDispatch }) => {
 			{notes.map((note) => {
 				return (
 					<div className="new-notes-container" key={note._id}>
-						<h1 className="note-title">{note.title}</h1>
-						<h4 className="note-content new-note">{note.content}</h4>
-						<div className="note-footer sm-text">
+						<h1
+							className="note-title"
+							style={{ backgroundColor: `${note.bgColor}` }}
+						>
+							{note.title}
+						</h1>
+						<h4
+							className="note-content new-note"
+							style={{ backgroundColor: `${note.bgColor}` }}
+						>
+							{note.content}
+						</h4>
+						<div
+							className="note-footer sm-text"
+							style={{ backgroundColor: `${note.bgColor}` }}
+						>
 							<h6>{note.createdTime}</h6>
+							<h6 className="tag-bg">{note.tags}</h6>
 							<div className="note-footer-icons">
-								<i className="fa-solid fa-palette"></i>
 								<i className="fa-solid fa-box-archive"></i>
 								<i
 									className="fa-solid fa-pen"
