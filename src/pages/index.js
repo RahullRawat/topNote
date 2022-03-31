fb667133a812902dd677d68130a20358d04f35bd
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-03-31 18:23:19 +0530

    feat :export achive page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,4 @@ export { Landing } from "./Landing/Landing";
 export { Login } from "./Authentication/Login";
 export { Signup } from "./Authentication/Signup";
 export { YourNotes } from "./YourNotes/YourNotes";
+export { Archive } from "./Archive/Archive";
