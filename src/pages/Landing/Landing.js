132ae248ad02c879ba8550d6691ebab2d4f68f78
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-04-01 15:40:59 +0530

    fix : added go to notes btn on landing page

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -2,8 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import heroImg from "../../assets/hero-img.png";
 import "./Landing.css";
+import { useAuth } from "../../context/AuthContext";
 
 const Landing = () => {
+	const { authState } = useAuth();
+	const { token } = authState;
 	return (
 		<div className="landing-container">
 			<div className="hero-container">
@@ -21,11 +24,18 @@ const Landing = () => {
 					</div>
 
 					<div className="auth-container">
-						<Link to="/signup">
-							<button className="btn btn-join">
-								Signup <span>- it's free</span>
-							</button>
-						</Link>
+						<div className="flex">
+							<Link to="/signup">
+								<button className="btn btn-join">
+									Signup <span>- it's free</span>
+								</button>
+							</Link>
+							{token && (
+								<Link to="/home">
+									<button className="btn btn-join">Go to notes</button>
+								</Link>
+							)}
+						</div>
 						<Link to="/login">
 							<button className="btn btn-signin">
 								Already have an account ?
