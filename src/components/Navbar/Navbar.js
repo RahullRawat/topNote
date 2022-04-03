6d3bd60bb19d5c82f43c773b40e06c46ada93296
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-04-03 18:40:34 +0530

    feat : added side Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import noteIcon from "../../assets/note-icon.png";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
@@ -6,32 +6,84 @@ import "./Navbar.css";
 
 const Navbar = () => {
 	const { authState, signOut } = useAuth();
-	return (
-		<nav className="navbar-container">
-			<div className="navbar-left">
-				<Link to="/" className="navbar-brand">
-					<h4 className="lg-text">TopNote </h4>
-					<img src={noteIcon} alt="note-logo" className="note-logo" />
-				</Link>
-			</div>
-			{!authState.userData && (
-				<div className="navbar-right">
-					<Link to="/login">
-						<button className="btn btn-join">Create Note</button>
-					</Link>
-				</div>
-			)}
+	const [mobileSidebar, setMobileSidebar] = useState(false);
 
-			{authState.userData && (
-				<div className="navbar-right">
-					<Link to="/">
-						<button className="btn btn-join" onClick={signOut}>
-							Logout
-						</button>
+	const showAside = () => {
+		setMobileSidebar(!mobileSidebar);
+	};
+
+	const closeSidebar = () => {
+		setMobileSidebar(!mobileSidebar);
+	};
+	return (
+		<>
+			<nav className="navbar-container">
+				<div className="navbar-left">
+					<i class="fa-solid fa-bars" onClick={showAside}></i>
+					<Link to="/" className="navbar-brand">
+						<h4 className="lg-text">TopNote </h4>
+						<img src={noteIcon} alt="note-logo" className="note-logo" />
 					</Link>
 				</div>
-			)}
-		</nav>
+
+				{!authState.userData && (
+					<div className="navbar-right">
+						<Link to="/login">
+							<button className="btn btn-join">Create Note</button>
+						</Link>
+					</div>
+				)}
+
+				{authState.userData && (
+					<div className="navbar-right">
+						<Link to="/">
+							<button className="btn btn-join" onClick={signOut}>
+								Logout
+							</button>
+						</Link>
+					</div>
+				)}
+			</nav>
+			<aside
+				className={
+					mobileSidebar ? `${"mobile-sidebar show-aside"}` : "mobile-sidebar"
+				}
+			>
+				<ul>
+					<Link to="/home">
+						<li onClick={closeSidebar}>
+							<i className="fa-solid fa-house"></i>Home
+						</li>
+					</Link>
+					<Link to="/yournotes">
+						<li onClick={closeSidebar}>
+							<i class="fa-solid fa-tags"></i>Your Notes
+						</li>
+					</Link>
+					<Link to="/archive">
+						<li onClick={closeSidebar}>
+							<i class="fa-solid fa-box-archive"></i>Archive
+						</li>
+					</Link>
+					<Link to="/trash">
+						<li onClick={closeSidebar}>
+							<i class="fa-solid fa-trash-can"></i>Trash
+						</li>
+					</Link>
+				</ul>
+				<div className="footer-profile">
+					<img
+						src="https://meta-ui.netlify.app/assets/first-avatar.jpg"
+						class="avatar avatar-sm"
+						alt="profile-avatar"
+					/>
+					<div className="profile details">
+						<h6 className="sm-text">{authState.userData.firstName}'s Notes</h6>
+						<span className="sm-text">@{authState.userData.firstName}</span>
+					</div>
+				</div>
+			</aside>
+		</>
 	);
 };
 
