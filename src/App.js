f7ee9d0d71283feb9d032ddafffe666ea7f86196
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-04-07 18:34:50 +0530

    feat : add requireAuth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { Navbar } from "./components/index";
+import { Navbar, RequireAuth } from "./components/index";
 import Mockman from "mockman-js";
 import {
 	Landing,
@@ -33,12 +33,40 @@ function App() {
 			<Navbar />
 			<Routes>
 				<Route path="/" element={<Landing />} />
-				<Route path="/home" element={<Homepage />} />
+				<Route
+					path="/home"
+					element={
+						<RequireAuth>
+							<Homepage />
+						</RequireAuth>
+					}
+				/>
 				<Route path="/login" element={<Login />} />
 				<Route path="/signup" element={<Signup />} />
-				<Route path="/yournotes" element={<YourNotes />} />
-				<Route path="/archive" element={<Archive />} />
-				<Route path="/trash" element={<Trash />} />
+				<Route
+					path="/yournotes"
+					element={
+						<RequireAuth>
+							<YourNotes />
+						</RequireAuth>
+					}
+				/>
+				<Route
+					path="/archive"
+					element={
+						<RequireAuth>
+							<Archive />
+						</RequireAuth>
+					}
+				/>
+				<Route
+					path="/trash"
+					element={
+						<RequireAuth>
+							<Trash />
+						</RequireAuth>
+					}
+				/>
 				<Route path="/mock" element={<Mockman />} />
 				<Route path="*" element={<Error404 />} />
 			</Routes>
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { Navbar } from "./Navbar/Navbar";
 export { Sidebar } from "./Sidebar/Sidebar";
 export { NoteInput } from "./NoteInput/NoteInput";
+export { RequireAuth } from "./RequireAuth/RequireAuth";
